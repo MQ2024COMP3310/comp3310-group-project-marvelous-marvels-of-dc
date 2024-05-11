d877a0e0e5ecae6787487406c515db73227ed839
Deb Kumar <59546212+MegaPanchamZ@users.noreply.github.com>
2024-05-12 03:20:44 +1000

    chore: Populate database with users and photos

--- initialise_db.py
+++ initialise_db.py
@@ -1,57 +1,43 @@
 from project import db, create_app, models
-from project.models import Photo
+from project.models import Photo, User
 
 def populate_db():
-   
+    
     session = db.session()
 
-    """ photo = Photo(name = 'William Warby', caption = 'Gentoo penguin', description = 'A penguin with an orange beak standing next to a rock.', file = 'william-warby-_A_vtMMRLWM.jpg') 
-    session.add(photo)
-    session.commit()
+    # Create Users
+    user1 = User(username='user1', password='password1', is_admin=False)
+    user2 = User(username='user2', password='password2', is_admin=False)
+    user3 = User(username='user3', password='password3', is_admin=False)
+    
+    # Adding Users to the Session
+    session.add(user1)
+    session.add(user2)
+    session.add(user3)
+    session.commit()  # Commit to assign IDs
+    
+    # Now Create Photos with a reference to the user id
+    photos = [
+        Photo(name='William Warby', caption='Gentoo penguin', description='A penguin with an orange beak standing next to a rock.', file='william-warby-_A_vtMMRLWM.jpg', user_id=user1.id),
+        Photo(name='Javier Patino Loira', caption='Common side-blotched lizard', description='A close up of a lizard on a rock.', file='javier-patino-loira-nortqDjv7ak.jpg', user_id=user1.id),
+        Photo(name='Jordie Rubies', caption='Griffin vulture flying', description='A large bird flying through a blue sky.', file='jordi-rubies-2wNkdL2oIyU.jpg', user_id=user2.id),
+        Photo(name='Jakub Neskora', caption='Jaguar', description='A close up of a leopard near a rock.', file='jakub-neskora-jloJvr74Fcc.jpg', user_id=user2.id),
+        Photo(name='William Warby', caption='Japanese macaque', description='A monkey sitting on top of a wooden post.', file='william-warby-ndWikw_TPfc.jpg', user_id=user3.id),
+        Photo(name='Ahmed Ali', caption='Berlin', description='An exciting part of Berlin. This place covers so many beautiful attractions in the city. From that spot you are already on the famous Oberbaumbrücke, you can see Molecule Man, and right behind me, you can see Berlin\'s beautiful skyline with the Fernsehturm right in the middle of it with the reflections of the spree.', file='ahmed-ali-Zl7bVVMEfg.jpg', user_id=user3.id),
+        Photo(name='Hanvin Cheong', caption='Nakano', description='A group of people walking across a street.', file='hanvin-cheong-9rBj8QYOL1Q.jpg', user_id=user1.id),
+        Photo(name='Ekaterina Bogdan', caption='Bologna', description='A bike parked next to a pole.', file='ekaterina-bogdan-BKJWsGB5h1s.jpg', user_id=user2.id),
+        Photo(name='Damian Ochrymowicz', caption='Nazare, Portugal', file='damian-ochrymowicz-GZQ7tKmEd9c.jpg', user_id=user3.id),
+        Photo(name='Dima DallAcqua', caption='Alcatraz Island', description='A close up of a green plant.', file='dima-dallacqua-U8TAGVPFJc4.jpg', user_id=user1.id),
+        Photo(name='Edgar', caption='Oporto, Portugal', description='A man sitting on a bench at a train station.', file='edgar-Q0g5Thf7Ank.jpg', user_id=user2.id),
+    ]
 
-    photo = Photo(name = 'Javier Patino Loira', caption = 'Common side-blotched lizard', description = 'A close up of a lizard on a rock.', file = 'javier-patino-loira-nortqDjv7ak.jpg') 
-    session.add(photo)
+    # Adding All Photos to the Session
+    session.add_all(photos)
     session.commit()
-
-    photo = Photo(name = 'Jordie Rubies', caption = 'Griffin vulture flying', description = 'A large bird flying through a blue sky.', file = 'jordi-rubies-2wNkdL2oIyU.jpg') 
-    session.add(photo)
-    session.commit()
-
-    photo = Photo(name = 'Jakub Neskora', caption = 'Jaguar', description = 'A close up of a leopard near a rock.', file = 'jakub-neskora-jloJvr74Fcc.jpg') 
-    session.add(photo)
-    session.commit()
-
-    photo = Photo(name = 'William Warby', caption = 'Japanese macaque', description = 'A monkey sitting on top of a wooden post.', file = 'william-warby-ndWikw_TPfc.jpg') 
-    session.add(photo)
-    session.commit()
-
-    photo = Photo(name = 'Ahmed Ali', caption = 'Berlin', description = 'An exciting part of Berlin. This place covers so many beautiful attractions in the city. From that spot you are already on the famous Oberbaumbrücke, you can see Molecule Man, and right behind me, you can see Berlin\'s beautiful skyline with the Fernsehturm right in the middle of it with the reflections of the spree.', file = 'ahmed-ali-Zl7bVVMEfg.jpg') 
-    session.add(photo)
-    session.commit()
-
-    photo = Photo(name = 'Hanvin Cheong', caption = 'Nakano', description = 'A group of people walking across a street.', file = 'hanvin-cheong-9rBj8QYOL1Q.jpg') 
-    session.add(photo)
-    session.commit()
-
-    photo = Photo(name = 'Ekaterina Bogdan', caption = 'Bologna', description = 'A bike parked next to a pole.', file = 'ekaterina-bogdan-BKJWsGB5h1s.jpg') 
-    session.add(photo)
-    session.commit()
-
-    photo = Photo(name = 'Damian Ochrymowicz', caption = 'Nazare, Portugal', file = 'damian-ochrymowicz-GZQ7tKmEd9c.jpg') 
-    session.add(photo)
-    session.commit()
-
-    photo = Photo(name = 'Dima DallAcqua', caption = 'Alcatraz Island', description = 'A close up of a green plant.', file = 'dima-dallacqua-U8TAGVPFJc4.jpg') 
-    session.add(photo)
-    session.commit()
-
-    photo = Photo(name = 'Edgar', caption = 'Oporto, Portugal', description = 'A man sitting on a bench at a train station.', file = 'edgar-Q0g5Thf7Ank.jpg') 
-    session.add(photo)
-    session.commit() """
 
 if __name__ == '__main__':
-  app = create_app()
-  with app.app_context():
-    db.drop_all()
-    db.create_all()
-
+    app = create_app()
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        populate_db()
