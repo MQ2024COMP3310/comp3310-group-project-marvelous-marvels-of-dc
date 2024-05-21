1afc12ddb517a93d6b11b37d965654057950c9ed
Deb Kumar <59546212+MegaPanchamZ@users.noreply.github.com>
2024-05-22 01:21:23 +1000

    feat: Add functionality to create comments on photos

--- project/main.py
+++ project/main.py
@@ -4,7 +4,7 @@ from flask import (
   current_app, make_response
 )
 from flask_login import current_user, login_required
-from .models import Photo
+from .models import Photo, Comment
 from sqlalchemy import asc, text
 from . import db
 import os
@@ -101,3 +101,22 @@ def deletePhoto(photo_id):
         flash('You do not have permission to delete this photo', 'error')
         return redirect(url_for('main.homepage'))
 
+
+# This is called when clicking on Comment. Goes to the comment page.
+@main.route('/photo/<int:photo_id>/comment', methods=['POST'])
+@login_required
+def create_comment(photo_id):
+    content = request.form.get('content')
+    if not content:
+        flash('Comment cannot be empty', 'error')
+    else:
+        new_comment = Comment(content=content, user_id=current_user.id, photo_id=photo_id)
+        db.session.add(new_comment)
+        db.session.commit()
+        flash('Comment posted successfully!', 'success')
+    return redirect(url_for('main.view_photo', photo_id=photo_id))
+
+@main.route('/photo/<int:photo_id>')
+def view_photo(photo_id):
+    photo = Photo.query.get_or_404(photo_id)
+    return render_template('photo.html', photo=photo)
